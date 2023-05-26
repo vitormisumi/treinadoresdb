34a6f74f70e89005b1cc66be273962ac0ae414d3
Vitor Misumi <vitormisumi@gmail.com>
2023-05-26 18:06:32 -0300

    feat: contact form

--- treineiros/src/routes/Footer.svelte
+++ treineiros/src/routes/Footer.svelte
@@ -6,30 +6,37 @@
     <h3>Entre em contato</h3>
     <p>
       Caso tenha ficado com alguma dúvida, tenha encontrado algum problema ou
-      tenha alguma sugestão de como podemos melhorar, nos envie uma mensagem e
-      responderemos o quanto antes.
+      tenha alguma sugestão, nos envie uma mensagem através do formulário
+      abaixo:
     </p>
-    <form action="contact-form-process.php">
-      <div id="name">
-        <label for="name">Nome</label><br />
-        <input type="text" id="name" />
-      </div>
-      <div id="email">
-        <label for="email">Email</label><br />
-        <input type="text" id="email" />
-      </div>
-      <div id="message">
-        <label for="message">Mensagem</label><br />
-        <textarea id="message" />
-      </div>
+    <form action="https://formsubmit.co/2d815fdbc924813927e605f56270e4a5" method="POST">
+      <input type="hidden" name="_subject" value="Nova mensagem de usuário">
+      <input type="hidden" name="_captcha" value="false">
+      <input type="hidden" name="_template" value="box">
+      <input type="text" name="_honey" style="display:none">
+      <input type="text" name="name" placeholder="Digite seu nome" required />
+      <input
+        type="email"
+        name="email"
+        placeholder="Digite seu email"
+        required
+      />
+      <textarea
+        name="message"
+        placeholder="Digite sua mensagem"
+        rows="5"
+        required
+      />
+      <input type="hidden" name="_next" value="https://treineiros.vercel.app/obrigado.html">
+      <button type="submit">Enviar</button>
     </form>
   </div>
-  <div id="menu">
-      <a href="/">Home</a>
-      <a href="/sobre">Sobre</a>
-      <a href="/h2h">H2H</a>
-      <a href="/rankings">Rankings</a>
-  </div>
+  <nav id="menu">
+    <a href="/">Home</a>
+    <a href="/sobre">Sobre</a>
+    <a href="/h2h">H2H</a>
+    <a href="/rankings">Rankings</a>
+  </nav>
 </section>
 
 <style>
@@ -37,18 +44,64 @@
     background-color: var(--main-color);
     margin: 0;
     padding: 5%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
   }
-  
-  h3, p, label, a {
+
+  h3,
+  p,
+  a {
     color: var(--background-color);
+    padding-bottom: 2vh;
+  }
+
+  a {
+    font-size: clamp(1rem, 2vw, 2rem);
+    padding: clamp();
+  }
+
+  a:hover {
+    color: var(--accent-color);
+  }
+
+  input::placeholder,
+  textarea::placeholder {
+    font-family: var(--main-font);
+    color: var(--accent-color);
+    opacity: 30%;
   }
 
   h3 {
     margin-top: 0;
   }
 
+  form {
+    display: grid;
+    gap: 10px;
+  }
+
+  p,
+  input,
   textarea {
-    width: min(75%, 300px);
-    height: 100px;
+    margin: 0;
+    text-align: justify;
+  }
+
+  button {
+    width: max(10vw, 100px);
+    justify-self: right;
+    margin-bottom: 30px;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+  }
+
+  @media (max-width: 768px) {
+    #footer {
+      grid-template-columns: 1fr;
+    }
   }
 </style>
---
+++ treineiros/src/routes/obrigado/+page.svelte
@@ -0,0 +1,8 @@
+<h4>Obrigado por entrar em contato. Retornaremos sua mensagem em breve!</h4>
+
+<style>
+    h4 {
+        margin: 40vh 5%;
+        text-align: center;
+    }
+</style>
